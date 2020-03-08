6698a2d4b5ad25ea0c96e534e93ac5125bb4b2d3
Nahuel Avalos <avalosnahuel@gmail.com>
2020-03-08 19:28:05 -0300

    Commit Refactor Tests 1.1.6

--- master/src/test/java/com/meli/mutantes/service/MutantesServiceTests.java
+++ master/src/test/java/com/meli/mutantes/service/MutantesServiceTests.java
@@ -1,31 +1,17 @@
 package com.meli.mutantes.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
-@ExtendWith(MockitoExtension.class)
 public class MutantesServiceTests {
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
 
     @Test
     public void ejemploChallenge() {
         String[] ejemploChallenge = {
                 "ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"
         };
-        //verify(humanRepository, never()).save(Mockito.any(Human.class));
         assertTrue(MutantesService.isMutant(ejemploChallenge));
-
     }
 
     @Test
@@ -33,72 +19,46 @@ public class MutantesServiceTests {
         String[] ejemploHumano = {
                 "ATGCGA","CGGTCC","TTATGT","AGAAGG","CTCCTA","TCACTG"
         };
-        //verify(humanRepository, never()).save(Mockito.any(Human.class));
         assertFalse(MutantesService.isMutant(ejemploHumano));
-
     }
 
     @Test
     public void ejemploFull() {
          String[] ejemploFull= {
             "ATGCGA", "CAGCGC", "TTCGGT", "ACAAGG", "CCCCTG", "TCACTG"};
-        //verify(humanRepository, never()).save(Mockito.any(Human.class));
          assertTrue(MutantesService.isMutant(ejemploFull));
-
     }
 
     @Test
     public void testHorizontalDNA() {
         String[] horizontalDNA = {
-                "CCCCC",
-                "ATCGT",
-                "ATCGT",
-                "CTTTT",
-                "CAGAC"
+                "CCCCC", "ATCGT", "ATCGT", "CTTTT", "CAGAC"
         };
-        //verify(humanRepository, never()).save(Mockito.any(Human.class));
         assertTrue(MutantesService.isMutant(horizontalDNA));
     }
 
     @Test
     public void testVerticualDNA() {
         String[] verticalDNA = {
-                "CGCGC",
-                "ATCGT",
-                "ATCGT",
-                "CTCTT",
-                "CAGAT"
+                "CGCGC", "ATCGT", "ATCGT", "CTCTT", "CAGAT"
         };
-      //  verify(humanRepository, never()).save(Mockito.any(Human.class));
         assertTrue(MutantesService.isMutant(verticalDNA));
     }
 
     @Test
     public void testDiagonalDNA() {
         String[] diagonalDNA = {
-                "CCACC",
-                "ACCGT",
-                "AACGT",
-                "CTACA",
-                "CAGAT"
+                "CCACC", "ACCGT", "AACGT", "CTACA", "CAGAT"
         };
-    //    verify(humanRepository, never()).save(Mockito.any(Human.class));
         assertTrue(MutantesService.isMutant(diagonalDNA));
     }
 
     @Test
     public void testAntiDiagonalDNA() {
         String[] antiDiagonalDNA = {
-                "CGCCC",
-                "ATCGT",
-                "ACGGT",
-                "CGCTT",
-                "GAGAG"
+                "CGCCC", "ATCGT", "ACGGT", "CGCTT", "GAGAG"
         };
-     //   verify(humanRepository, never()).save(Mockito.any(Human.class));
-        //verify(Mockito.any(DnaDTO.class));
         assertTrue(MutantesService.isMutant(antiDiagonalDNA));
-
     }
 
     @Test
